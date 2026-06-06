8b2ddfc4ffec057c61c6e68b49acba4470f5683b
gof2-decomp <decomp@local>
2026-06-07 02:28:02 +0800

    chore: remove subagent scratch candidate sources; gitignore the pattern

--- work/classes/AEMath/src/_cand_v15.cpp
+++
@@ -1,9 +0,0 @@
-#include "class.h"
-namespace AbyssEngine { namespace AEMath {
-bool operator<(const Vector &lhs, const Vector &rhs) {
-    const float *ly = &lhs.y;
-    if (!(lhs.x < rhs.x)) return false;
-    if (!(ly[0] < rhs.y)) return false;
-    return ly[1] < rhs.z;
-}
-} }
--- work/classes/AEMath/src/_cand_v16.cpp
+++
@@ -1,9 +0,0 @@
-#include "class.h"
-namespace AbyssEngine { namespace AEMath {
-bool operator<(const Vector &lhs, const Vector &rhs) {
-    const Vector *p = &lhs;
-    if (!(lhs.x < rhs.x)) return false;
-    if (!(p->y < rhs.y)) return false;
-    return p->z < rhs.z;
-}
-} }
