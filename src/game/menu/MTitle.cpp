2c68e1e1982914bf197055e9df003bdad46ed570
gof2-decomp <decomp@local>
2026-07-21 02:00:00 +0200

    MTitle pass: reconstruct OnKeyPress/OnKeyRelease UB stubs + improve OnInitialize/OnRelease/OnRender3D/OnRender2D from Ghidra. stubs 11->9, linked_exact 2127->2130, byte_exact 1115 unchanged. RATCHET PASS.

--- src/game/menu/MTitle.cpp
+++ src/game/menu/MTitle.cpp
@@ -7,9 +7,9 @@
 #include "engine/render/PaintCanvas.h"
 #include "game/core/Globals.h"
 
-static Layout *g_currentLayout = nullptr;
-static ImageFactory *g_imageFactory = nullptr;
-static FModSound *g_sound = nullptr;
+static Layout **g_currentLayout = &Globals::layout;
+static ImageFactory **g_imageFactory = &Globals::imageFactory;
+static FModSound **g_sound = &Globals::sound;
 
 MTitle::MTitle() {
     this->renderPriority = 100;
@@ -24,7 +24,7 @@ int MTitle::OnInitialize() {
     canvas->Image2DCreate(7000, this->logoImage);
     canvas->Image2DCreate(0x1b59, this->logoImage2);
 
-    g_sound->play(0x91, (Vector *) 0, (Vector *) 0, 0.0f);
+    (*g_sound)->play(0x91, (Vector *) 0, (Vector *) 0, 0.0f);
 
     this->step = 0;
     this->timer = 0;
@@ -37,16 +37,19 @@ void MTitle::OnRelease() {
 
     Globals::globals->loadFont(GameText::getLanguage());
 
-    if (g_currentLayout != 0) {
-        g_currentLayout->reload();
-        g_imageFactory->reload();
+    if (*g_currentLayout != nullptr) {
+        (*g_currentLayout)->reload();
+        (*g_imageFactory)->reload();
+        (*g_currentLayout)->initTip();
     }
 }
 
 long long MTitle::OnKeyPress(long long key, long long mod) {
+    return 0LL;
 }
 
 long long MTitle::OnKeyRelease(long long key, long long mod) {
+    return 0LL;
 }
 
 void MTitle::OnTouchBegin(int x, int y) {
@@ -59,13 +62,12 @@ void MTitle::OnUpdate() {
 }
 
 void MTitle::OnRender2D() {
-    PaintCanvas *canvas = Globals::Canvas;
-    canvas->Begin2d();
-    canvas->SetColor((unsigned int) -1);
+    this->paintCanvas->Begin2d();
+    Globals::Canvas->SetColor((unsigned int) -1);
 
-    g_currentLayout->drawBG();
-    g_currentLayout->drawHeader();
-    g_currentLayout->drawEmptyFooter(0);
+    (*g_currentLayout)->drawBG();
+    (*g_currentLayout)->drawHeader();
+    (*g_currentLayout)->drawEmptyFooter(0);
 
     int delta = (int) this->applicationManager->GetElapsedTimeMillis();
     if (delta > 50)
@@ -96,15 +98,15 @@ void MTitle::OnRender2D() {
     }
 
     int color = (int) (fade * 255.0f) - 0x100;
-    canvas->SetColor((unsigned int) color);
-    canvas->DrawImage2D((unsigned int) image, 0, 0, (unsigned char) 0, (unsigned char) 0x44);
-    canvas->End2d();
+    this->paintCanvas->SetColor((unsigned int) color);
+    Globals::Canvas->DrawImage2D((unsigned int) image, 0, 0, (unsigned char) 0x44, (unsigned char) 0x44);
+    this->paintCanvas->End2d();
 }
 
 void MTitle::OnRender3D() {
-    PaintCanvas *canvas = Globals::Canvas;
-    canvas->ClearBuffer(0xff);
-    canvas->Begin3d();
+    Globals::Canvas->ClearBuffer(0xff);
+    this->paintCanvas->Begin3d();
+    this->paintCanvas->End3d();
 }
 
 void MTitle::OnTouchEnd(int x, int y) {
